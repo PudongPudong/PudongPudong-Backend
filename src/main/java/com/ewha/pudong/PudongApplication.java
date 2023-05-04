456815edde6d53b9ba75e945ad0618b3ca0786b3
YOONJI KIM <stritegdc@gmail.com>
2023-05-04 15:37:22 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/ewha/pudong/PudongApplication.java
@@ -0,0 +1,13 @@
+package com.ewha.pudong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PudongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PudongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewha/pudong/PudongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewha.pudong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PudongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
