f0c0d6e8838c90e4d503e39a7d2359b3256b8f0f
YOONJI KIM <stritegdc@gmail.com>
2023-05-04 16:02:30 +0900

    feat: 기본 user domain 생성

---
+++ src/main/java/com/ewha/pudong/domain/User.java
@@ -0,0 +1,56 @@
+package com.ewha.pudong.domain;
+
+import com.ewha.pudong.config.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="user")
+public class User extends BaseEntity{
+    @Id @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotNull
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column(columnDefinition = "TEXT")
+    private String image;
+
+    @Column
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    private String provider; //어떤 OAuth인지
+
+    private String providerId;
+
+    @Builder
+    public User(String username, String password, String email, String image, Role role, String provider, String providerId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.image = image;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+}
